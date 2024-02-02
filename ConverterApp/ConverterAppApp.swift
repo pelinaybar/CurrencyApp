e75482af9f5adef14a481ce1cae0431b099f009c
pelinaybar <aybarpelin00@gmail.com>
2024-02-02 15:41:31 +0300

    Initial Commit

---
+++ ConverterApp/ConverterAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ConverterAppApp.swift
+//  ConverterApp
+//
+//  Created by Pelin Aybar on 2.02.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ConverterAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ConverterAppUITests/ConverterAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConverterAppUITestsLaunchTests.swift
+//  ConverterAppUITests
+//
+//  Created by Pelin Aybar on 2.02.2024.
+//
+
+import XCTest
+
+final class ConverterAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
